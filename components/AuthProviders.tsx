c8d6fcfec5cd350d8a736fed381e9ceecb9323a5
Alex Suvorov <compjkee@gmail.com>
2023-08-23 21:38:45 +0300

    fixed auth providers

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,46 +1,46 @@
-'use client'
+"use client";
 
-import {useState,useEffect} from 'react'
-import {signIn , getProviders} from 'next-auth/react'
-import Button from './Button'
+import { useState, useEffect } from "react";
+import { signIn, getProviders } from "next-auth/react";
+import Button from "./Button";
+
 
 type Provider = {
-   id : string
-   name : string
-   type : string
-   sigInUrl : string
-   callbackUrl : string
-   signinUrlParams : Record<string , string> | null
-}
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams?: Record<string, string> | undefined;
+};
 
-type Providers = Record<string , Provider>;
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
+  const [providers, setproviders] = useState<Providers | null>(null);
+  console.log(providers);
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders();
+      setproviders(res);
+    };
+    fetchProviders();
+  }, []);
 
-   const [providers, setproviders] = useState<Providers | null>(null);
- console.log(providers)
- useEffect(() => {
-   const fetchProviders = async () => {
-      const res = await getProviders()
-      setproviders(res)
-   }
-   fetchProviders()
- },[])
-
-   if(providers) {
-      return (
-        <div>
-          {Object.values(providers).map((provider: Provider) => (
-            <Button 
-            key={provider.id} 
+  if (providers) {
+    return (
+      <div>
+        {Object.values(providers).map((provider: Provider) => (
+          <Button
+            key={provider.id}
             handleClick={() => signIn(provider?.id)}
-            title='Sign in'
-            type='button'
-            />
-          ))}
-        </div>
-      );
-   }
-}
- 
+            title="Sign in"
+            type="button"
+          />
+        ))}
+      </div>
+    );
+  }
+};
+
 export default AuthProviders;
